c2508f4f45c432adba99a04d0ebcaea84945ec0d
Meredith Edwards <meredith.edwards1771@gmail.com>
2016-10-30 18:40:55 -0400

    WIP: working on paginating queries to Muse API

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,19 @@
 class JobsController < ApplicationController
 
   def index
-    JobConstructor.new.perform
-    @jobs = Job.all
+    JobConstructor.new.perform(params[:page])
+    if params[:page] == '1'
+      @jobs = Job.all[0..19]
+    elsif params[:page] == '2'
+      @jobs = Job.all[20..39]
+    elsif params[:page] == '3'
+      @jobs = Job.all[40..59]
+    end
+  end
+
+  private
+
+  def job_params
+    params.permit(:page)
   end
 end
--- app/models/job_constructor.rb
+++ app/models/job_constructor.rb
@@ -1,10 +1,14 @@
 class JobConstructor
 
-  def perform
+  def perform(page)
     gateway = MuseGateway.new
-    results = gateway.fetch('https://api-v2.themuse.com/jobs?page=1')['results']
-    results.each do |result|
-      Job.new(result['name'], result['refs']['landing_page'], result['company']['name'])
+    results = gateway.fetch("https://api-v2.themuse.com/jobs?page=#{page}")['results']
+    build_jobs(results)
+  end
+
+  def build_jobs(job_data)
+    job_data.each do |job|
+      Job.new(job['name'], job['refs']['landing_page'], job['company']['name'])
     end
   end
 end
